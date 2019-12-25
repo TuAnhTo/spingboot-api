2cb61d778c77ec40d9049360c0107e3aee6f09c8
Tô Tú <totu@Tos-MacBook-Pro.local>
2019-12-25 09:30:09 +0700

    add method delete Blogs

--- src/main/java/com/codegym/controller/BlogController.java
+++ src/main/java/com/codegym/controller/BlogController.java
@@ -1,16 +1,12 @@
 package com.codegym.controller;
 
 import com.codegym.model.Blog;
-//import com.codegym.model.Customer;
 import com.codegym.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
@@ -20,15 +16,15 @@ public class BlogController {
     @Autowired
     private BlogService blogService;
 
-    @RequestMapping(value = "/blogs/", method = RequestMethod.GET )
-    public ResponseEntity<List<Blog>> listAllBlogs(){
+    @RequestMapping(value = "/blogs/", method = RequestMethod.GET)
+    public ResponseEntity<List<Blog>> listAllBlogs() {
         try {
 
             List<Blog> blogs = blogService.findAll();
-            if (blogs.isEmpty()){
+            if (blogs.isEmpty()) {
                 return new ResponseEntity<List<Blog>>(HttpStatus.NO_CONTENT);
             }
-            return new ResponseEntity<List<Blog>>(blogs,HttpStatus.OK);
+            return new ResponseEntity<List<Blog>>(blogs, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<List<Blog>>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -36,13 +32,49 @@ public class BlogController {
     }
 
 
-
     @RequestMapping(value = "/blogs/", method = RequestMethod.POST)
-    public ResponseEntity<Void> createBlogs(@RequestBody Blog blog, UriComponentsBuilder uriComponentsBuilder){
-        System.out.println(" Create Blogs: " +blog.getTitle() + blog.getContent());
+    public ResponseEntity<Void> createBlogs(@RequestBody Blog blog, UriComponentsBuilder uriComponentsBuilder) {
+        System.out.println(" Create Blogs: " + blog.getTitle() + blog.getContent());
         blogService.saveBlog(blog);
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setLocation(uriComponentsBuilder.path("/blogs/{id}").buildAndExpand(blog.getId()).toUri());
-        return new ResponseEntity<Void>(httpHeaders,HttpStatus.CREATED);
+        return new ResponseEntity<Void>(httpHeaders, HttpStatus.CREATED);
     }
+
+
+    @RequestMapping(value = "/blogs/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Blog> updateBlog(@PathVariable("id") long id, @RequestBody Blog blog) {
+        System.out.println("Updating Blogs " + id);
+
+        Blog currentBlog = blogService.findById(id);
+
+        if (currentBlog == null) {
+            System.out.println("Blogs with id " + id + " not found");
+            return new ResponseEntity<Blog>(HttpStatus.NOT_FOUND);
+        }
+
+        currentBlog.setTitle(blog.getTitle());
+        currentBlog.setContent(blog.getContent());
+        currentBlog.setId(blog.getId());
+
+        blogService.saveBlog(currentBlog);
+        return new ResponseEntity<Blog>(currentBlog, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/blogs/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Blog> deleteBlog(@PathVariable("id") long id) {
+        System.out.println("Fetching & Deleting Blog with id " + id);
+
+        Blog blog = blogService.findById(id);
+        if (blog == null) {
+            System.out.println("Unable to delete. Blogs with id " + id + " not found");
+            return new ResponseEntity<Blog>(HttpStatus.NOT_FOUND);
+        }
+
+        blogService.remove(id);
+        return new ResponseEntity<Blog>(HttpStatus.NO_CONTENT);
+    }
+
+
 }
